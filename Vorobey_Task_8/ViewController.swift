60209e2343c58bf13d66c899114adf80dc71a746
roman1pr <proman11pr@gmail.com>
2023-09-24 10:27:18 +0300

    Done 8

--- Vorobey_Task_8/ViewController.swift
+++ Vorobey_Task_8/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//На экране UINavigationController с большим заголовком и скрол вью. Справа от заголовка картинка аватара.
 
+//- Картинка SF Symbols person.crop.circle.fill. Размер 36x36pt.
+//- Картинка двигается и обрезается вместе с заголовком.
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    private lazy var image: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "person.crop.circle.fill")
+        image.tintColor = .gray
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: 1000)
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Avatar"
+        
+        view = scrollView
+        view.backgroundColor = .white
+        
+        setupAvatar()
     }
+    
+    
+    private func setupAvatar(){
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        
+        // Находим вью с большим заголовком
+        if let largeTitleView = navigationBar.subviews.first(where: { NSStringFromClass($0.classForCoder).contains("UINavigationBarLargeTitleView") }) {
+            // Добавляем картинку на вью
+            largeTitleView.addSubview(image)
 
+            // Устанавливаем констрейнты для картинки
+            NSLayoutConstraint.activate([
+                image.topAnchor.constraint(equalTo: largeTitleView.bottomAnchor, constant: 20),
+                image.bottomAnchor.constraint(equalTo: image.superview?.bottomAnchor ?? navigationBar.bottomAnchor, constant: -10),
+                
+                image.rightAnchor.constraint(equalTo: largeTitleView.rightAnchor, constant: -20),
+                
+                image.heightAnchor.constraint(equalToConstant: 36),
+                image.widthAnchor.constraint(equalToConstant: 36)
+            ])
+            
 
+        }
+    }
 }
-
