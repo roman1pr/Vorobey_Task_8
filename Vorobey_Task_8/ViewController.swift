be43b061f3dc301423375b00c2973f2b9056880e
roman1pr <proman11pr@gmail.com>
2023-09-24 10:09:19 +0300

    Initial Commit

---
+++ Vorobey_Task_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vorobey_Task_8
+//
+//  Created by Roman Priiskalov on 24.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
